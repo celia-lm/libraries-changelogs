5b3b160d5bfde1c2e6f4fe7e81544607f43ce22a
workspaceuser <workspaceuser@plot.ly>
2026-04-14 08:24:02 +0000

    One Click Deploy - 1776155041916

--- pages/resources.py
+++ pages/resources.py
@@ -1,5 +1,5 @@
 # https://github.com/MrNaif2018/changelog-checker/tree/master
-# _generate_package_report function:
+# _generate_package_report function: 
 # https://github.com/MrNaif2018/changelog-checker/blob/master/changelog_checker/core.py#L84C9-L84C33
 
 from dash import dcc, html
